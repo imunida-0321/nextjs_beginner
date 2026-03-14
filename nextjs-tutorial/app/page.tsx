0aaeebc9d5d8a54e633734aff3bdc23ae9e5f5d5
imunida <hirokazu.official@gmail.com>
2026-03-14 19:21:00 +0900

    11.client Componentsとは

--- nextjs-tutorial/app/page.tsx
+++ nextjs-tutorial/app/page.tsx
@@ -1,6 +1,25 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useEffect } from 'react'
+
+// これでクライアントコンポーネントと認識される
+// つけたからと言ってなんでも自由に書けるわけではない
 
 export default function Home() {
-	return <h1>Hello World</h1>
+	// window is not definedとエラーが出る
+	// 実行できない理由はNext.jsではクライアントコンポーネントであってもサーバ側でHTMLを生成しようとする
+	// その際にwindow.alertも一緒に実行されてしまい、そこでエラーになる
+
+	// window.alert('Hello')
+
+	// 解決するにはuseEffectを使用する
+	// 画面表示後に実行される
+	useEffect(() => {
+		window.alert('Hello')
+	}, [])
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<button onClick={() => alert('Clicked')}>Click Me!</button>
+		</div>
+	)
 }
