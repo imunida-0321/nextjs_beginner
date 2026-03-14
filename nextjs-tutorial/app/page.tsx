5c15e83ccb80ec8eb4d1d64eadead5fd924de449
imunida <hirokazu.official@gmail.com>
2026-03-14 19:50:26 +0900

    12. Server Componentsを使った、Next.jsのデータ取得

--- nextjs-tutorial/app/page.tsx
+++ nextjs-tutorial/app/page.tsx
@@ -1,25 +1,24 @@
-'use client'
-import { useEffect } from 'react'
+type Post = {
+	id: number
+	title: string
+}
 
-// これでクライアントコンポーネントと認識される
-// つけたからと言ってなんでも自由に書けるわけではない
-
-export default function Home() {
-	// window is not definedとエラーが出る
-	// 実行できない理由はNext.jsではクライアントコンポーネントであってもサーバ側でHTMLを生成しようとする
-	// その際にwindow.alertも一緒に実行されてしまい、そこでエラーになる
-
-	// window.alert('Hello')
-
-	// 解決するにはuseEffectを使用する
-	// 画面表示後に実行される
-	useEffect(() => {
-		window.alert('Hello')
-	}, [])
+export default async function Home() {
+	// fetchには独自の強力な機能がある、キャッシュの機能
+	// 通常は外部のAPIにデータを取得しにいくのをページを表示するために毎回実行してしまうと通信が発生し時間がかかる
+	// Next.jsのfetch関数は、サーバー側にこっそり保存しておき、次に同じデータが必要になったときは
+	// 外部APIではなくNextのサーバー常に保存しておいたデータを返してくれる→これにより無駄な通信を防ぎ高速な表示が可能となる
+	// キャッシュではなく新しいデータを1から取得する細かいキャッシュの制御方法については、後続のレクチャーで解説
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+	const posts: Post[] = await res.json()
 	return (
 		<div>
-			<h1>Hello World</h1>
-			<button onClick={() => alert('Clicked')}>Click Me!</button>
+			<h1>記事一覧</h1>
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
 		</div>
 	)
 }
