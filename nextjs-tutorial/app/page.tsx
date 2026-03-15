f85708e5f8f3ed412178dd8c348bea01a89ec6fd
imunida <hirokazu.official@gmail.com>
2026-03-16 00:25:43 +0900

    19. Revalidationとは？（途中）

--- nextjs-tutorial/app/page.tsx
+++ nextjs-tutorial/app/page.tsx
@@ -1,15 +1,3 @@
-// use Cache使用前
-// 実際に重いデータを取得してるわけではないがsetTimeoutで擬似的に再現
-// async function getHeavyData() {
-// 	await new Promise((resolve) => setTimeout(resolve, 3000))
-// 	return '重いデータの取得完了'
-// }
-
-// export default async function Home() {
-// 	const data = await getHeavyData()
-// 	return <h1>{data}</h1>
-// }
-
 // use Cache使用
 async function getHeavyData() {
 	// これでgetHeavyDataのキャッシュが保存される
